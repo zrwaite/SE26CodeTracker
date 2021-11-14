ecc009253bc24a0434c8266cc84ea5319aeba2ce
Zac Waite <129032699zw@gmail.com>
2021-11-14 00:47:50 -0500

    Standard response

---
+++ backend/src/models/response.ts
@@ -0,0 +1,14 @@
+export default class response {
+    success: boolean;
+    status: number;
+    errors: string[];
+    response: object;
+    constructor(status=400, errors=[], response={}, success=false, auth={}) {
+        this.success = success;
+        this.status = status;
+        //this.auth = auth;
+        this.errors = [];
+        this.errors.push(...errors);
+        this.response = response;
+    }
+}
