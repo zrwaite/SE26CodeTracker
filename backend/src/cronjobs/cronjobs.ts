2a88f6756aed6078f255e32a1c5e7ece9fbef22c
Zac Waite <129032699zw@gmail.com>
2021-12-29 16:00:25 -0500

    Code parsing

--- backend/src/cronjobs/cronjobs.ts
+++ backend/src/cronjobs/cronjobs.ts
@@ -1,7 +1,13 @@
 import cron from "node-cron";
 import dailyCodeData from "./dailyCodeData";
+import {createCodeStats} from "../modules/parseData";
+import {getAllCodeData} from "../modules/getWakatimeInfo";
 // dailyCodeData();
+const testFunc = async () =>{
+	let answer = await getAllCodeData("sec_BkehSGo2t3VyfnmDS2WZWfwUPHdPL0c4hLIkos7dUtpKrdnzCTvUPH1rkHP6f49ENp0tLVEN2YtrOByq");
+}
 const cronjobs = () => {
+	testFunc();
 	cron.schedule('1 * * * *', () => {
 		// dailyCodeData();
 	});
