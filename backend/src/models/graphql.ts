1ca8123dc23e96551a091347e8e681e50b750f13
Zac Waite <129032699zw@gmail.com>
2021-12-29 00:14:19 -0500

    Some graphql integration

---
+++ backend/src/models/graphql.ts
@@ -0,0 +1,14 @@
+import { SchemaComposer } from 'graphql-compose';
+const schemaComposer = new SchemaComposer();
+
+import { UserQuery, UserMutation } from './user';
+
+schemaComposer.Query.addFields({
+    ...UserQuery,
+});
+
+schemaComposer.Mutation.addFields({
+    ...UserMutation,
+});
+
+export default schemaComposer.buildSchema();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,9 @@ import env from "dotenv";
 //import jwt from 'express-jwt';
 //import jwks from 'jwks-rsa';
 import {response} from "./models/response"; //Created pre-formatted uniform response
+import { graphqlHTTP } from "express-graphql";
+
+
 
 const app = express();
 
@@ -17,7 +20,8 @@ app.use(express.json());
 
 
 // routes
-import apiRoute from "./route/api.route";
+import apiRoute from "./routes/api.route";
+import { GraphQLSchema } from "graphql";
 
 // api routing
 app.use("/api", apiRoute);
@@ -31,6 +35,22 @@ app.get("/backend/*", (req, res) => {
 	let result = new response(403, [], {response: "nice try buddy"});
 	res.status(result.status).json(result); //Doesn't allow people access to backend files
 });
+
+import graphqlSchema  from "./models/graphql";
+const extensions = ({context}:any) => {
+	return {
+		runTime: Date.now() - context.startTime
+	}
+}
+app.use("/graphql", graphqlHTTP((request:any) => {
+	return {
+		context: { startTime: Date.now() },
+		graphql: true,
+		schema: graphqlSchema,
+		extensions,
+	}
+}));
+
 
 // app.get("/files/*", getFile);
 
