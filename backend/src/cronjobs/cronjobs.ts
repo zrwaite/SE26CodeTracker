9c9d4168cc3659add77b993ac27c7289b474ebbc
Zac Waite <129032699zw@gmail.com>
2022-01-01 10:59:14 -0500

    Begin integration Update user information

--- backend/src/cronjobs/cronjobs.ts
+++ backend/src/cronjobs/cronjobs.ts
@@ -1,9 +1,6 @@
 import cron from "node-cron";
 import dailyCodeData from "./dailyCodeData";
-import {createCodeStats} from "../modules/parseData";
-import {getAllCodeData} from "../modules/getWakatimeInfo";
 const cronjobs = () => {
-	// testFunc();
 	cron.schedule('1 0 * * *', () => {
 		dailyCodeData();
 	});
