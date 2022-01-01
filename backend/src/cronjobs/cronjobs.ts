9215dd4e31c90f8a42b39393677227fdd6894928
Zac Waite <129032699zw@gmail.com>
2021-12-31 22:11:52 -0500

    update daily code stats

--- backend/src/cronjobs/cronjobs.ts
+++ backend/src/cronjobs/cronjobs.ts
@@ -2,11 +2,10 @@ import cron from "node-cron";
 import dailyCodeData from "./dailyCodeData";
 import {createCodeStats} from "../modules/parseData";
 import {getAllCodeData} from "../modules/getWakatimeInfo";
-// dailyCodeData();
 const cronjobs = () => {
 	// testFunc();
-	cron.schedule('1 * * * *', () => {
-		// dailyCodeData();
+	cron.schedule('1 0 * * *', () => {
+		dailyCodeData();
 	});
 }
 export default cronjobs;
--- backend/src/modules/parseData.ts
+++ backend/src/modules/parseData.ts
@@ -3,9 +3,69 @@ interface apiStats {
 	name:string;
 	total_seconds: number
 }
+
+// const parseDayStats = async (all
+const parseDayStats = async (day:any, startingStats:stats = {
+	wins: 0,
+	total_time: 0,
+	day_time: 0,
+	week_time: 0,
+	editors: [],
+	languages: [],
+	os: [],
+	days: []
+}, addDay:boolean = false, addWeek:boolean = false) => {
+	const currentDate:string = day.range.date;
+	const dayStats:dayObject = {
+		time: 0,
+		date: currentDate
+	}
+	startingStats.day_time=0;
+	day.editors.forEach((editor:apiStats)=>{
+		let name = editor.name;
+		let time = editor.total_seconds;
+		let found = false;
+		for (let i=0; i<startingStats.editors.length; i++){
+			if (name != startingStats.editors[i].name) continue;
+			startingStats.editors[i].time += time;
+			found = true;
+			break;
+		} 
+		if (!found) startingStats.editors.push({name:name, time:time});
+	})
+	day.languages.forEach((language:apiStats)=>{
+		let name = language.name;
+		let time = language.total_seconds;
+		dayStats.time+=time;
+		let found = false;
+		for (let i=0; i<startingStats.languages.length; i++){
+			if (name != startingStats.languages[i].name) continue;
+			startingStats.languages[i].time += time;
+			found = true;
+			break;
+		} 
+		if (!found) startingStats.languages.push({name:name, time:time});
+	})
+	day.operating_systems.forEach((os:apiStats)=>{
+		let name = os.name;
+		let time = os.total_seconds;
+		let found = false;
+		for (let i=0; i<startingStats.os.length; i++){
+			if (name != startingStats.os[i].name) continue;
+			startingStats.os[i].time += time;
+			found = true;
+			break;
+		} 
+		if (!found) startingStats.os.push({name:name, time:time});
+	})
+	startingStats.total_time+=dayStats.time;
+	startingStats.days.push(dayStats);
+	if (addDay) startingStats.day_time = dayStats.time;
+	if (addWeek) startingStats.week_time += dayStats.time;
+	return startingStats;
+}
 const createCodeStats = async (allStats:any[]) =>{
-	console.log(JSON.stringify(allStats[0]));
-	const codeStats:stats = {
+	let codeStats:stats = {
 		wins: 0,
 		total_time: 0,
 		day_time: 0,
@@ -15,58 +75,11 @@ const createCodeStats = async (allStats:any[]) =>{
 		os: [],
 		days: []
 	}
-	let found = false;
 	for (let numDay=0; numDay<allStats.length; numDay++){
 		const day = allStats[numDay];
-		const currentDate:string = day.range.date;
-		const dayStats:dayObject = {
-			time: 0,
-			date: currentDate
-		}
-		codeStats.day_time=0;
-		day.editors.forEach((editor:apiStats)=>{
-			let name = editor.name;
-			let time = editor.total_seconds;
-			found = false;
-			for (let i=0; i<codeStats.editors.length; i++){
-				if (name != codeStats.editors[i].name) continue;
-				codeStats.editors[i].time += time;
-				found = true;
-				break;
-			} 
-			if (!found) codeStats.editors.push({name:name, time:time});
-		})
-		day.languages.forEach((language:apiStats)=>{
-			let name = language.name;
-			let time = language.total_seconds;
-			dayStats.time+=time;
-			found = false;
-			for (let i=0; i<codeStats.languages.length; i++){
-				if (name != codeStats.languages[i].name) continue;
-				codeStats.languages[i].time += time;
-				found = true;
-				break;
-			} 
-			if (!found) codeStats.languages.push({name:name, time:time});
-		})
-		day.operating_systems.forEach((os:apiStats)=>{
-			let name = os.name;
-			let time = os.total_seconds;
-			found = false;
-			for (let i=0; i<codeStats.os.length; i++){
-				if (name != codeStats.os[i].name) continue;
-				codeStats.os[i].time += time;
-				found = true;
-				break;
-			} 
-			if (!found) codeStats.os.push({name:name, time:time});
-		})
-		codeStats.total_time+=dayStats.time;
-		codeStats.days.push(dayStats);
-		if (allStats.length-1 <= numDay) codeStats.day_time = dayStats.time;
-		if (allStats.length-7 <= numDay) codeStats.week_time += dayStats.time;
+		codeStats = await parseDayStats(day, codeStats, (allStats.length-1 <= numDay), (allStats.length-7 <= numDay));
 	}
 	return codeStats;
 }
 
-export {createCodeStats};
+export {createCodeStats, parseDayStats};
