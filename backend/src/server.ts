05ed74e2917b32cb1323f050c6d37c66442e19b8
Zac Waite <129032699zw@gmail.com>
2022-01-15 17:07:26 -0400

    Dockerized, serve frontend, ready for beta launch

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import env from "dotenv";
-// import path from "path";
+import path from "path";
+import fs from "fs";
 //import jwt from 'express-jwt';
 //import jwks from 'jwks-rsa';
 import {response} from "./models/response"; //Created pre-formatted uniform response
@@ -35,7 +36,22 @@ app.get("/backend/*", (req, res) => {
 	res.status(result.status).json(result); //Doesn't allow people access to backend files
 });
 
+const folderPath = path.join(__dirname, "../../frontend");
 
-// app.get("/files/*", getFile);
+app.get("/", (req, res) => {
+	if (fs.existsSync(folderPath + "/index.html")) res.status(200).sendFile("/index.html", {root: folderPath});
+	else res.status(404).json("404 File not found");
+})
 
-export default app; //Export server for use in index.js
+app.get("*", (req, res) => {
+	if (fs.existsSync(folderPath + req.url + "/index.html")) {
+		res.status(200).sendFile(req.url  + "/index.html", {root: folderPath});
+		console.log(folderPath + req.url + "/index.html");
+	} else if (fs.existsSync(folderPath + req.url)) {
+		res.status(200).sendFile(req.url, {root: folderPath});
+		console.log(folderPath + req.url);
+	}
+	else res.status(404).json("404 File not found");
+})
+
+export default app; //Export server for use in index.ts
