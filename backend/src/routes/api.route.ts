b534aea5afe933a0f9b0eb20b8fe511eeea2468b
Zac Waite <129032699zw@gmail.com>
2022-01-18 20:30:24 -0500

    Group endpoint

--- backend/src/routes/api.route.ts
+++ backend/src/routes/api.route.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import userCtrl from '../api/user';
+import groupCtrl from '../api/group';
 const router = express.Router();
 router.route('/user')
 	.get(userCtrl.getUser)
 	.post(userCtrl.postUser)
 	.put(userCtrl.putUser)
 	.delete(userCtrl.deleteUser)
+router.route('/group')
+	.get(groupCtrl.getGroup)
+	.post(groupCtrl.postGroup)
+	.put(groupCtrl.putGroup)
+	.delete(groupCtrl.deleteGroup)
 export default router;
-
