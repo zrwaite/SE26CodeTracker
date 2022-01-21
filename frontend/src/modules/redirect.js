0944dc8cce3b41883a406926f0992134af45e1e3
Zac Waite <129032699zw@gmail.com>
2022-01-20 23:04:22 -0500

    Remove compiled js

--- frontend/src/modules/redirect.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const redirect = (loggedInPage) => {
-    if (loggedInPage === (!getCookie("username") || !getCookie("token"))) {
-        if (loggedInPage)
-            window.location.href = "../signin";
-        else
-            window.location.href = "../stats";
-    }
-};
